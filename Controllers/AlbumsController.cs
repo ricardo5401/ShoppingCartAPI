57ee66cafb399edd9a32313a57f612be95f190a7
Ricardo Garcia <ricardo.izasiga@gmail.com>
2017-06-09 13:05:36 -0500

    'add_email_confirmation'

--- Controllers/AlbumsController.cs
+++ Controllers/AlbumsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShoppingCartApi.Models;
+using ShoppingCartApi.Data;
 using ShoppingCartApi.Repositories;
 
 namespace ShoppingCartApi.Controllers
